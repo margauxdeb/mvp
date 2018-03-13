3b66f9de8dcdfbaf1491882647862ebf210ae73c
margauxdeb <margauxdeb@gmail.com>
2018-03-13 04:55:54 -0400

    sdsdsdsdssdsds#

--- simple-app/src/app/article/article.component.ts
+++ simple-app/src/app/article/article.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
+
 export class ArticleComponent implements OnInit {
+  private _title: string;
+  private _content: string;
+  private _author: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this._title = "First Article";
+    this._content = "Hello World";
+    this._author = "Auteur Test";
   }
+
+  public title(): string {
+    return this._title;
+  }
+
+  public content(): string {
+    return this._content;
+  }
+  
+  public author(): string {
+    return this._author;
+  }
+
+  ngOnInit() {}
 
 }
